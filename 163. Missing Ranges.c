ea331c0e42c4aa260a85c9a8dd63d79940f03f12
Victor <vli02@hotmail.com>
2017-09-07 23:04:42 -0700

    re-organized the code.

--- 163. Missing Ranges.c
+++ 163. Missing Ranges.c
@@ -11,65 +11,55 @@ For example, given [0, 1, 3, 50, 75], lower = 0 and upper = 99, return ["2", "4-
  * Return an array of size *returnSize.
  * Note: The returned array must be malloced, assume caller calls free().
  */
-char** findMissingRanges(int* nums, int numsSize, int lower, int upper, int* returnSize) {
-    char **p, *buff;
-    int psz, hi, i;
-    
-    psz = 10;
-    p = malloc(psz * sizeof(char *));
-    //assert(p);
-​
-    *returnSize = 0;
-    
-    i = 0;
-    while (i < numsSize) {
-        if (lower < nums[i]) {
-            // add lower ... min(nums[i] - 1, upper) to the buff;
-            buff = calloc(32, sizeof(char)); // 32 bytes
-            // assert(buff);
-            hi = (nums[i] - 1) < upper ? (nums[i] - 1) : upper;
-            if (lower == hi) {
-                sprintf(buff, "%d", lower);
-            } else {
-                sprintf(buff, "%d->%d", lower, hi);
-            }
-            if (*returnSize == psz) {
-                psz = psz * 2;
-                p = realloc(p, psz * sizeof(char *));
-                //assert(p);
-            }
-            p[*returnSize] = buff;
-            (*returnSize) ++;
-        }
-        if (nums[i] >= upper) goto done;
-        lower = nums[i] + 1;
-        i ++;
-    }
-    if (lower <= upper) {
-        // add lower ... min(nums[i] - 1, upper) to the buff;
-        buff = calloc(32, sizeof(char)); // 32 bytes
-        // assert(buff);
-        if (lower == upper) {
-            sprintf(buff, "%d", lower);
-        } else {
-            sprintf(buff, "%d->%d", lower, upper);
-        }
-        if (*returnSize == psz) {
-            psz ++;
-            p = realloc(p, psz * sizeof(char *));
-            //assert(p);
-        }
-        p[*returnSize] = buff;
-        (*returnSize) ++;
-    }
-done:
-    if (*returnSize == 0) {
-        free(p);
-        p = NULL;
-    }
-    return p;
+typedef struct {
+    char **p;
+    int sz;
+    int n;
+} res_t;
+void add2res(res_t *res, int l, int h) {
+    char *buff;
+    buff = calloc(32, sizeof(char)); // 32 bytes
+    // assert(buff);
+    if (l == h) {
+        sprintf(buff, "%d", l);
+    } else {
+        sprintf(buff, "%d->%d", l, h);
+    }
+    if (!res->sz) {
+        res->sz = 10;
+        res->p = malloc(res->sz * sizeof(char *));
+        //assert(res->p);
+    } else if (res->sz == res->n) {
+        res->sz *= 2;
+        res->p = realloc(res->p, res->sz * sizeof(char *));
+        //assert(res->p);
+    }
+    res->p[res->n ++] = buff;
 }
-
+char** findMissingRanges(int* nums, int numsSize, int lower, int upper, int* returnSize) {
+    res_t res = { 0 };
+    int psz, hi, i;
+    
+    for (i = 0; i < numsSize; i ++) {
+        if (lower < nums[i]) {
+            // add (lower ... nums[i] - 1) to the buff;
+            hi = nums[i] - 1;
+            add2res(&res, lower, hi);
+        }
+        if (nums[i] >= upper) goto done;
+        lower = nums[i] + 1;
+    }
+    
+    if (lower <= upper) {
+        // add (lower ... upper) to the buff;
+        add2res(&res, lower, upper);
+    }
+    
+done:
+    *returnSize = res.n;
+    
+    return res.p;
+}
 
 /*
 Difficulty:Medium
