3ef2ca7554d4570ed459bfa93492acefd18eaa68
Victor <vli02@hotmail.com>
2017-09-09 12:38:21 -0700

    Simplified the code.

--- 279. Perfect Squares.c
+++ 279. Perfect Squares.c
@@ -11,62 +11,31 @@ For example, given n = 12, return 3 because 12 = 4 + 4 + 4; given n = 13, return
 Credits:Special thanks to @jianchao.li.fighter for adding this problem and creating all test cases.
 */
 
-void dfs(int n, int *m, int d) {
-    int nn, k;
-    
-    if (*m != -1 && d > *m) return;
-    
-    k = sqrt(n);
-    
-    if (k == 1) {
-        d += n - 1;
-        if (*m == -1 || *m > d) {
-            *m = d;
-        }
-        return;
-    } else if (n == k * k) {
-        if (*m == -1 || *m > d) {
-            *m = d;
-        }
-        return;
-    }
-    
-    while (k > 0) {  // k is the one which makes the remaining as smallest
-        nn = n - k * k;
-        dfs(nn, m, d + 1);
-        k --;
-    }
-}
 int numSquares(int n) {
-    int m = -1;
-#if 0
-    dfs(n, &m, 1);
-#else
-    int i, j, k;
-    int *dp;
-    
-    dp = malloc((n + 1) * sizeof(int));
-    //assert(dp);
-    
-    dp[0] = 0;
-    for (i = 1; i <= n; i ++) {
-        k = dp[i - 1] + 1;
-        j = 2;
-        while (i >= j * j) {  // after j ^ 2
-            if (k > dp[i - j * j] + 1) {
-                k = dp[i - j * j] + 1;
-            }
-            j ++;
-        }
-        dp[i] = k;
-    }
-    
-    m = dp[n];
-    free(dp);
-#endif
-    return m;
+    int i, j, k;
+    int *dp;
+    
+    dp = malloc((n + 1) * sizeof(int));
+    //assert(dp);
+    
+    dp[0] = 0;
+    for (i = 1; i <= n; i ++) {
+        k = dp[i - 1] + 1;
+        j = 2;
+        while (i >= j * j) {  // after j ^ 2
+            if (k > dp[i - j * j] + 1) {
+                k = dp[i - j * j] + 1;
+            }
+            j ++;
+        }
+        dp[i] = k;
+    }
+    
+    k = dp[n];
+    free(dp);
+    
+    return k;
 }
-
 
 /*
 Difficulty:Medium
