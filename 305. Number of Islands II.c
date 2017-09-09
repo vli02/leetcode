9ce53fa9c5bd9a3a458c343600611981313842b9
Victor <vli02@hotmail.com>
2017-09-08 18:36:39 -0700

    simplified the code.

--- 305. Number of Islands II.c
+++ 305. Number of Islands II.c
@@ -45,89 +45,77 @@ Can you do it in time complexity O(k log mn), where k is the length of the posit
  * Note: The returned array must be malloced, assume caller calls free().
  */
 #define IDX(I, L, J) ((I) * (L) + J)
-void expand(int *land, int m, int n, int x, int y, int g) {
-    const int offset[4][2] = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
-    int i, j, o, s;
-​
-    land[IDX(x, n, y)] = g;
-    
-    for (o = 0; o < 4; o ++) {
-        i = x + offset[o][0];
-        j = y + offset[o][1];
-        if (i >= 0 && i < m &&
-            j >= 0 && j < n) {
-            s = land[IDX(i, n, j)];
-            if (s != 0 && s != g) {
-                expand(land, m, n, i, j, g);
-            }
-        }
-    }
+void expand(int *k, int *land, int m, int n, int x, int y, int g) {
+    const int offset[4][2] = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
+    int i, j, o;
+    
+    for (o = 0; o < 4; o ++) {
+        i = x + offset[o][0];
+        j = y + offset[o][1];
+        if (i >= 0 && i < m &&
+            j >= 0 && j < n &&
+            land[IDX(i, n, j)] &&
+            land[IDX(i, n, j)] != g) {
+            if (k) (*k) --;
+            land[IDX(i, n, j)] = g;
+            expand(NULL, land, m, n, i, j, g);
+        }
+    }
 }
 int fill_it(int k, int *s, int *land, int m, int n, int x, int y) {
-    const int offset[4][2] = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
-    int i, j, o;
-    int g;
-    
-    g = 0;
-    for (o = 0; o < 4; o ++) {
-        i = x + offset[o][0];
-        j = y + offset[o][1];
-        if (i >= 0 && i < m &&
-            j >= 0 && j < n &&
-            g < land[IDX(i, n, j)]) {
-            g = land[IDX(i, n, j)];
-        }
-    }
-    
-    if (g == 0) { // all surrounding are water
-        (*s) ++;
-        k ++;
-        land[IDX(x, n, y)] = *s;
-    } else {
-        land[IDX(x, n, y)] = g;
-        for (o = 0; o < 4; o ++) {
-            i = x + offset[o][0];
-            j = y + offset[o][1];
-            if (i >= 0 && i < m &&
-                j >= 0 && j < n &&
-                land[IDX(i, n, j)] &&
-                land[IDX(i, n, j)] != g) {
-                expand(land, m, n, i, j, g);
-                k --;
-            }
-        }
-    }
-    
-    return k;
-}
-​
-int* numIslands2(int m, int n, int** positions, int positionsRowSize, int positionsColSize, int* returnSize) {
-    int *land;
-    int *p;
-    int i, x, y;
-    int k, s;
-​
-    land = calloc(m * n, sizeof(int));
-    p = malloc(positionsRowSize * sizeof(int));
-    //assert(land && p);
-​
-    k = s = 0;
-    for (i = 0; i < positionsRowSize; i ++) {
-        x = positions[i][0];
-        y = positions[i][1];
-        if (x < m && y < n && land[x * n + y] == 0) {
-            k = fill_it(k, &s, land, m, n, x, y);
-        }
-        p[i] = k;
-    }
-    
-    free(land);
-    
-    *returnSize = positionsRowSize;
-    
-    return p;
+    const int offset[4][2] = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
+    int i, j, o;
+    int g;
+    
+    g = 0;
+    for (o = 0; o < 4; o ++) {
+        i = x + offset[o][0];
+        j = y + offset[o][1];
+        if (i >= 0 && i < m &&
+            j >= 0 && j < n &&
+            g < land[IDX(i, n, j)]) {
+            g = land[IDX(i, n, j)];
+        }
+    }
+    
+    if (g == 0) { // all surrounding are water
+        (*s) ++;
+        k ++;
+        land[IDX(x, n, y)] = *s;
+    } else {
+        land[IDX(x, n, y)] = g;
+        expand(&k, land, m, n, x, y, g);
+    }
+    
+    return k;
 }
 
+int* numIslands2(int m, int n, int** positions, int positionsRowSize, int positionsColSize, int* returnSize) {
+    int *land;
+    int *p;
+    int i, x, y;
+    int k, s;
+
+    land = calloc(m * n, sizeof(int));
+    p = malloc(positionsRowSize * sizeof(int));
+    //assert(land && p);
+
+    k = s = 0;
+    for (i = 0; i < positionsRowSize; i ++) {
+        x = positions[i][0];
+        y = positions[i][1];
+        if (x < m && y < n && land[x * n + y] == 0) {
+            k = fill_it(k, &s, land, m, n, x, y);
+        }
+        p[i] = k;
+    }
+    
+    free(land);
+    
+    *returnSize = positionsRowSize;
+    
+    return p;
+}
 
 /*
 Difficulty:Hard
