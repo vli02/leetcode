da11827da5e9a0b7307358307c6986e84b25c8ff
Victor <vli02@hotmail.com>
2017-09-09 09:49:28 -0700

    simplified the code.

--- 15. 3Sum.c
+++ 15. 3Sum.c
@@ -18,56 +18,64 @@ A solution set is:
  * Return an array of arrays of size *returnSize.
  * Note: The returned array must be malloced, assume caller calls free().
  */
+typedef struct res_s {
+    int **p;
+    int sz;
+    int n;
+} res_t;
+void add2res(res_t *res, int a, int b, int c) {
+    int *buff = malloc(3 * sizeof(int));
+    //assert(buff);
+    buff[0] = a;
+    buff[1] = b;
+    buff[2] = c;
+    
+    if (res->sz == 0) {
+        res->sz = 10;
+        res->p = malloc(res->sz * sizeof(int **));
+        //assert(res->p);
+    } else if (res->sz == res->n) {
+        res->sz *= 2;
+        res->p = realloc(res->p, res->sz * sizeof(int **));
+        //assert(res->p);
+    }
+    res->p[res->n ++] = buff;
+}
 int cmp(const void *a, const void *b) {
-    return *(int *)a - *(int *)b;
+    return *(int *)a - *(int *)b;
 }
 int** threeSum(int* nums, int numsSize, int* returnSize) {
-    int i, x, y, k;
-    int **p, sz, n;
-    
-    sz = 10;
-    p = malloc(sz * sizeof(int *));
-    //assert(p);
-    n = 0;
-    
-    qsort(nums, numsSize, sizeof(int), cmp);
-    
-    for (i = 0; i < numsSize - 2; i ++) {
-        if (i != 0 && nums[i] == nums[i - 1]) continue;
-        x = i + 1;
-        y = numsSize - 1;
-        while (x < y) {
-            k = nums[i] + nums[x] + nums[y];
-            if (k == 0) {
-                // found one
-                if (n == sz) {
-                    sz *= 2;
-                    p = realloc(p, sz * sizeof(int *));
-                    //assert(p);
-                }
-                p[n] = malloc(3 * sizeof(int));
-                p[n][0] = nums[i];
-                p[n][1] = nums[x];
-                p[n][2] = nums[y];
-                n ++;
-                x ++;
-                while (x < y && nums[x] == nums[x - 1]) x ++;
-                y --;
-                while (x < y && nums[y] == nums[y + 1]) y --;
-            } else if (k < 0) {
-                x ++;
-                while (x < y && nums[x] == nums[x - 1]) x ++;
-            } else {
-                y --;
-                while (x < y && nums[y] == nums[y + 1]) y --;
-            }
-        }
-    }
-    
-    *returnSize = n;
-    return p;
+    int i, x, y, k;
+    res_t res = { 0 };
+    
+    qsort(nums, numsSize, sizeof(int), cmp);
+    
+    for (i = 0; i < numsSize - 2; i ++) {
+        if (i != 0 && nums[i] == nums[i - 1]) continue;
+        x = i + 1;
+        y = numsSize - 1;
+        while (x < y) {
+            k = nums[i] + nums[x] + nums[y];
+            if (k == 0) {
+                // found one
+                add2res(&res, nums[i], nums[x], nums[y]);
+                x ++;
+                while (x < y && nums[x] == nums[x - 1]) x ++;
+                y --;
+                while (x < y && nums[y] == nums[y + 1]) y --;
+            } else if (k < 0) {
+                x ++;
+                while (x < y && nums[x] == nums[x - 1]) x ++;
+            } else {
+                y --;
+                while (x < y && nums[y] == nums[y + 1]) y --;
+            }
+        }
+    }
+    
+    *returnSize = res.n;
+    return res.p;
 }
-
 
 /*
 Difficulty:Medium
