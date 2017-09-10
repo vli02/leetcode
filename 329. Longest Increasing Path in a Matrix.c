f81d2832da60a2f8b693d9b7112b7a67095f3c5c
Victor <vli02@hotmail.com>
2017-09-10 14:21:44 -0700

    restructured

--- 329. Longest Increasing Path in a Matrix.c
+++ 329. Longest Increasing Path in a Matrix.c
@@ -41,69 +41,68 @@ Credits:Special thanks to @dietpepsi for adding this problem and creating all te
 
 #define IDX(I, J, L) ((I) * (L) + (J))
 int depth(int **matrix, int rowsz, int colsz, int *dep, int row, int col) {
-    int s, x, z, y;
-    int d = 0;
-    
-    d = dep[IDX(row, col, colsz)];
-    if (d != -1) {
-        return d;
-    }
-    
-    if (row == 0 || matrix[row][col] <= matrix[row - 1][col]) {
-        s = 1;
-    } else {
-        s = depth(matrix, rowsz, colsz, dep, row - 1, col) + 1;
-    }
-    
-    if (row == rowsz - 1 || matrix[row][col] <= matrix[row + 1][col]) {
-        x = 1;
-    } else {
-        x = depth(matrix, rowsz, colsz, dep, row + 1, col) + 1;
-    }
-    
-    if (col == 0 || matrix[row][col] <= matrix[row][col - 1]) {
-        z = 1;
-    } else {
-        z = depth(matrix, rowsz, colsz, dep, row, col - 1) + 1;
-    }
-    
-    if (col == colsz - 1 || matrix[row][col] <= matrix[row][col + 1]) {
-        y = 1;
-    } else {
-        y = depth(matrix, rowsz, colsz, dep, row, col + 1) + 1;
-    }
-    
-    d = s;
-    d = d > x ? d : x;
-    d = d > z ? d : z;
-    d = d > y ? d : y;
-    dep[IDX(row, col, colsz)] = d;
-    
-    return d;
-}
-​
-int longestIncreasingPath(int** matrix, int matrixRowSize, int matrixColSize) {
-    int d, max = 0;
-    int i, j;
-    int *dep;
-    
-    if (matrixRowSize <= 0 || matrixColSize <= 0) return 0;
-    
-    dep = malloc(matrixRowSize * matrixColSize * sizeof(int));
-    //assert(dep);
-    memset((void *)dep, -1, matrixRowSize * matrixColSize * sizeof(int));
-    for (i = 0; i < matrixRowSize; i ++) {
-        for (j = 0; j < matrixColSize; j ++) {
-            d = depth(matrix, matrixRowSize, matrixColSize, dep, i, j);
-            max = max < d ? d : max;
-        }
-    }
-    
-    free(dep);
-​
-    return max;
+    int s, x, z, y;
+    int d = 0;
+    
+    d = dep[IDX(row, col, colsz)];
+    if (d) {
+        return d;
+    }
+    
+    if (row == 0 || matrix[row][col] <= matrix[row - 1][col]) {
+        s = 1;
+    } else {
+        s = depth(matrix, rowsz, colsz, dep, row - 1, col) + 1;
+    }
+    
+    if (row == rowsz - 1 || matrix[row][col] <= matrix[row + 1][col]) {
+        x = 1;
+    } else {
+        x = depth(matrix, rowsz, colsz, dep, row + 1, col) + 1;
+    }
+    
+    if (col == 0 || matrix[row][col] <= matrix[row][col - 1]) {
+        z = 1;
+    } else {
+        z = depth(matrix, rowsz, colsz, dep, row, col - 1) + 1;
+    }
+    
+    if (col == colsz - 1 || matrix[row][col] <= matrix[row][col + 1]) {
+        y = 1;
+    } else {
+        y = depth(matrix, rowsz, colsz, dep, row, col + 1) + 1;
+    }
+    
+    d = s;
+    d = d > x ? d : x;
+    d = d > z ? d : z;
+    d = d > y ? d : y;
+    dep[IDX(row, col, colsz)] = d;
+    
+    return d;
 }
 
+int longestIncreasingPath(int** matrix, int matrixRowSize, int matrixColSize) {
+    int i, j;
+    int *dep;
+    int d, max = 0;
+    
+    if (matrixRowSize <= 0 || matrixColSize <= 0) return 0;
+    
+    dep = calloc(matrixRowSize * matrixColSize, sizeof(int));
+    //assert(dep);
+    
+    for (i = 0; i < matrixRowSize; i ++) {
+        for (j = 0; j < matrixColSize; j ++) {
+            d = depth(matrix, matrixRowSize, matrixColSize, dep, i, j);
+            max = max < d ? d : max;
+        }
+    }
+    
+    free(dep);
+
+    return max;
+}
 
 /*
 Difficulty:Hard
