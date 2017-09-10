c5c4154b86d68da7af5d78cb3ae55265069c0047
Victor <vli02@hotmail.com>
2017-09-10 14:09:45 -0700

    restructured the code.

--- 320. Generalized Abbreviation.c
+++ 320. Generalized Abbreviation.c
@@ -14,75 +14,84 @@ Given word = "word", return the following list (order does not matter):
  * Return an array of size *returnSize.
  * Note: The returned array must be malloced, assume caller calls free().
  */
-void abbr(char *word, int l, int start, int k, int *mask, char ***p, int *sz, int *n) {
-    int i, j, x;
-    char *buff;
-    
-    if (k == 0) {
-        // generate the abbr
-        buff = malloc((l + 1) * sizeof(char));
-        //assert(buff);
-        j = x = 0;
-        for (i = 0; i < l; i ++) {
-            if (mask[i]) {
-                x ++;
-            } else {
-                if (x) {
-                    j += sprintf(&buff[j], "%d", x);
-                    x = 0;
-                }
-                buff[j ++] = word[i];
-            }
-        }
-        if (x) {
-            j += sprintf(&buff[j], "%d", x);
-            x = 0;
-        }
-        buff[j] = 0;
-        if (*n == *sz) {
-            (*sz) *= 2;
-            (*p) = realloc(*p, (*sz) * sizeof(char *));
-            //assert(*p);
-        }
-        (*p)[*n] = buff;
-        (*n) ++;
-        return;
-    }
-    
-    for (i = start; i < l; i ++) {
-        mask[i] = 1;
-        abbr(word, l, i + 1, k - 1, mask, p, sz, n);
-        mask[i] = 0;
-    }
+typedef struct {
+    char **p;
+    int sz;
+    int n;
+} res_t;
+void add2res(res_t *res, char *buff) {
+    if (!res->sz) {
+        res->sz = 100;
+        res->p = malloc(res->sz * sizeof(char *));
+        //assert(res->p);
+        res->n = 0;
+    } else if (res->sz == res->n) {
+        res->sz *= 2;
+        res->p = realloc(res->p, res->sz * sizeof(char *));
+        //assert(res->p);
+    }
+    res->p[res->n ++] = buff;
+}
+char *abbr(char *word, int l, int *mask) {
+    int i, j, x;
+    char *buff;
+    // generate the abbr
+    buff = malloc((l + 1) * sizeof(char));
+    //assert(buff);
+    j = x = 0;
+    for (i = 0; i < l; i ++) {
+        if (mask[i]) {
+            x ++;
+        } else {
+            if (x) {
+                j += sprintf(&buff[j], "%d", x);
+                x = 0;
+            }
+            buff[j ++] = word[i];
+        }
+    }
+    if (x) {
+        j += sprintf(&buff[j], "%d", x);
+        x = 0;
+    }
+    buff[j] = 0;
+    return buff;
+}
+void dfs(char *word, int l, int start, int k, int *mask, res_t *res) {
+    int i;
+    
+    if (k == 0) {
+        add2res(res, abbr(word, l, mask));
+        return;
+    }
+    
+    for (i = start; i < l; i ++) {
+        mask[i] = 1;
+        dfs(word, l, i + 1, k - 1, mask, res);
+        mask[i] = 0;
+    }
 }
 char** generateAbbreviations(char* word, int* returnSize) {
-    int i, l;
-    int *mask;
-​
-    int sz, n;
-    char **p;
-    
-    *returnSize = 0;
-    if (!word) return NULL;
-    
-    sz = 100;
-    p = malloc(sz * sizeof(char *));
-    //assert(p);
-​
-    n = 0;
-    l = strlen(word);
-    mask = calloc(l, sizeof(int));
-    //assert(mask);
-    
-    for (i = 0; i <= l; i ++) {
-        abbr(word, l, 0, i, mask, &p, &sz, &n);
-    }
-    
-    *returnSize = n;
-    
-    return p;
+    int i, l;
+    int *mask;
+    
+    res_t res = { 0 };
+    
+    *returnSize = 0;
+    if (!word) return NULL;
+    
+    l = strlen(word);
+    mask = calloc(l, sizeof(int));
+    //assert(mask);
+    
+    for (i = 0; i <= l; i ++) {
+        dfs(word, l, 0, i, mask, &res);
+    }
+    
+    *returnSize = res.n;
+    
+    return res.p;
 }
-
 
 /*
 Difficulty:Medium
