6a6fe686705db839d0677c82e64a47e696155ece
Victor <vli02@hotmail.com>
2017-09-20 21:56:43 -0700

    simplified

--- 36. Valid Sudoku.c
+++ 36. Valid Sudoku.c
@@ -15,78 +15,49 @@ A valid Sudoku board (partially filled) is not necessarily solvable. Only the fi
 */
 
 #define VERIFY_HASH(I, J) do { \
-    c = board[I][J]; \
-    if (c >= '1' && c <= '9') { \
-        b = 1 << (c - '1'); \
-        if (hash & b) return false; \
-        hash |= b; \
-    }\
+    c = board[I][J]; \
+    if (c >= '1' && c <= '9') { \
+        b = 1 << (c - '1'); \
+        if (hash & b) return false; \
+        hash |= b; \
+    }\
 } while (0)
-​
-#define IDX(I, J) ((I) / 3 * (boardColSize + 2) / 3 + (J) / 3)
-​
-bool isValidSudoku(char** board, int boardRowSize, int boardColSize) {
-    int hash;
-    int i, j, m, n;
-    char c;
-    int b;
-    
-#if 0
-    for (i = 0; i < boardRowSize; i ++) {
-        hash = 0;
-        for (j = 0; j < boardColSize; j ++) {
-            VERIFY_HASH(i, j);
-        }
-    }
-    
-    for (j = 0; j < boardColSize; j ++) {
-        hash = 0;
-        for (i = 0; i < boardRowSize; i ++) {
-            VERIFY_HASH(i, j);
-        }
-    }
-    
-    for (i = 0; i < boardRowSize / 3; i ++) {
-        for (j = 0; j < boardColSize / 3; j ++) {
-            hash = 0;
-            for (m = 0; m < 3 && i * 3 + m < boardRowSize; m ++) {
-                for (n = 0; n < 3 && j * 3 + n < boardColSize; n ++) {
-                    VERIFY_HASH(i * 3 + m, j * 3 + n);
-                }
-            }
-        }
-    }
-#else
-    int *hc, *hs, *buff;
-    buff = calloc((boardColSize + ((boardRowSize + 2) / 3 * (boardColSize + 2) / 3)), sizeof(int));
-    //assert(buff);
-    hc = &buff[0];
-    hs = &buff[boardColSize];
-    
-    for (i = 0; i < boardRowSize; i ++) {
-        hash = 0;
-        for (j = 0; j < boardColSize; j ++) {
-            c = board[i][j];
-            if (c < '1' || c > '9') continue;
-            b = 1 << (c - '1');
-            if ((hash & b) ||           // verify failed on current line
-                (hc[j] & b) ||          // verify failed on current column
-                (hs[IDX(i, j)] & b)) {  // failed on current nestled square
-                free(buff);
-                return false;
-            }
-            hash |= b;
-            hc[j] |= b;
-            hs[IDX(i, j)] |= b;
-        }
-    }
-    free(buff);
-#endif
-    
-    return true;
-}
-​
 
+#define IDX(I, J) ((I) / 3 * (boardColSize + 2) / 3 + (J) / 3)
+
+bool isValidSudoku(char** board, int boardRowSize, int boardColSize) {
+    int hash;
+    int i, j, m, n;
+    char c;
+    int b;
+    
+    int *hc, *hs, *buff;
+    buff = calloc((boardColSize + ((boardRowSize + 2) / 3 * (boardColSize + 2) / 3)), sizeof(int));
+    //assert(buff);
+    hc = &buff[0];
+    hs = &buff[boardColSize];
+    
+    for (i = 0; i < boardRowSize; i ++) {
+        hash = 0;
+        for (j = 0; j < boardColSize; j ++) {
+            c = board[i][j];
+            if (c < '1' || c > '9') continue;
+            b = 1 << (c - '1');
+            if ((hash & b) ||           // verify failed on current line
+                (hc[j] & b) ||          // verify failed on current column
+                (hs[IDX(i, j)] & b)) {  // failed on current nestled square
+                free(buff);
+                return false;
+            }
+            hash |= b;
+            hc[j] |= b;
+            hs[IDX(i, j)] |= b;
+        }
+    }
+    free(buff);
+    
+    return true;
+}
 
 /*
 Difficulty:Medium
