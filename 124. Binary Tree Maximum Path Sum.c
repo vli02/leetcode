f4cc97e6589fc452aac25566aab301baf8ca3b90
Victor <vli02@hotmail.com>
2017-09-21 23:02:22 -0700

    simplified

--- 124. Binary Tree Maximum Path Sum.c
+++ 124. Binary Tree Maximum Path Sum.c
@@ -21,32 +21,37 @@ Return 6.
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
- *     int val;
- *     struct TreeNode *left;
- *     struct TreeNode *right;
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
  * };
  */
 int maxsum(struct TreeNode *node, int *max) {
-    int l, r, k, s;
-    if (!node) return 0;
-    l = maxsum(node->left, max);
-    r = maxsum(node->right, max);
-    l = l > 0 ? l : 0;
-    r = r > 0 ? r : 0;
-    k = node->val + (l > r ? l : r);  // current node + left side or right side
-    s = node->val + l + r;            // current node + both sides
-    if (*max < s) *max = s;
-    return k;
+    int l, r, k, s;
+    
+    if (!node) return 0;
+    
+    l = maxsum(node->left, max);
+    r = maxsum(node->right, max);
+    
+    l = l > 0 ? l : 0;                  // ignore negative
+    r = r > 0 ? r : 0;
+    
+    s = node->val + l + r;              // current node + both sides
+    if (*max < s) *max = s;
+    
+    k = node->val + (l > r ? l : r);    // current node + one side only
+    return k;
 }
 int maxPathSum(struct TreeNode* root) {
-    int max;
-    if (root) {
-        max = root->val;
-        maxsum(root, &max);
-    } else {
-        max = 0;
-    }
-    return max;
+    int max;
+    if (root) {
+        max = root->val;
+        maxsum(root, &max);
+    } else {
+        max = 0;
+    }
+    return max;
 }
 
 
