78dde038f93d75c244956d2d95749be02bcc7f2f
Victor <vli02@hotmail.com>
2017-10-05 13:56:11 -0700

    simplified

--- 300. Longest Increasing Subsequence.c
+++ 300. Longest Increasing Subsequence.c
@@ -17,64 +17,63 @@ Follow up: Could you improve it to O(n log n) time complexity?
 Credits:Special thanks to @pbrother for adding this problem and creating all test cases.
 */
 
-int lower_bound(int *lens, int start, int end, int k) {
-    int mid;
-    
-    while (start < end) {
-        mid = start + (end - start) / 2;
-        if (lens[mid] == k) return mid;
-        if (lens[mid] > k) end = mid - 1;
-        else start = mid + 1;
-    }
-    
-    // replace the last one which is not less than k, or
-    // append the k to the end if k is the greatest one
-    if (lens[end] >= k) return end;
-    
-    return end + 1;
+int lower_bound(int *lens, int start, int end, int last, int k) {
+    int mid;
+    
+    if (start > end) {
+        // locate the first one which is not less than current number
+        if (start <= last && lens[start] > lens[last]) {
+            start ++;
+        }
+        return start;
+    }
+    
+    mid = start + (end - start) / 2;
+    
+    if (lens[mid] == k) return mid;
+    
+    if (lens[mid] > k) return lower_bound(lens, start, mid - 1, last, k);
+    
+    return lower_bound(lens, mid + 1, end, last, k);
 }
 int lengthOfLIS(int* nums, int numsSize) {
-    int *lens, max, i, j;
-    
-    if (numsSize == 0) return 0;
-    
-    lens = malloc(numsSize * sizeof(int));
-    //assert(lens);
-    
-    max = 1;
-    
-#if 0   // O(n^2)   19ms
-    for (i = 0; i < numsSize; i ++) {
-        lens[i] = 1;
-        for (j = 0; j < i; j ++) {
-            if (nums[j] < nums[i] &&
-                lens[i] < lens[j] + 1) {
-                lens[i] = lens[j] + 1;
-            }
-        }
-        if (max < lens[i]) max = lens[i];
-    }
-#else   // O(nlogn) 3ms
-    int start, end;
-​
-    start = end = 0;
-    
-    lens[0] = nums[0];
-    for (i = 1; i < numsSize; i ++) {
-        j = lower_bound(lens, start, end, nums[i]);
-        lens[j] = nums[i];
-        if (end < j) {
-            end = j;
-        }
-    }
-    max = end + 1;
+    int *lens, max, i, j;
+    
+    if (numsSize == 0) return 0;
+    
+    lens = malloc(numsSize * sizeof(int));
+    //assert(lens);
+    
+    max = 1;
+    
+#if 0   // O(n^2)   19ms
+    for (i = 0; i < numsSize; i ++) {
+        lens[i] = 1;
+        for (j = 0; j < i; j ++) {
+            if (nums[j] < nums[i] &&
+                lens[i] < lens[j] + 1) {
+                lens[i] = lens[j] + 1;
+            }
+        }
+        if (max < lens[i]) max = lens[i];
+    }
+#else   // O(nlogn) 3ms
+    int end = 0;
+    
+    lens[end] = nums[0];
+    
+    for (i = 1; i < numsSize; i ++) {
+        j = lower_bound(lens, 0, end, end, nums[i]);
+        lens[j] = nums[i];
+        end = (j > end) ? j : end;
+    }
+    max = end + 1;
 #endif
-    
-    free(lens);
-    
-    return max;
+    
+    free(lens);
+    
+    return max;
 }
-
 
 /*
 Difficulty:Medium
