ce5427df91a1b72742393cc8f9e832d4fe4ba103
Victor <vli02@hotmail.com>
2017-10-06 15:27:05 -0700

    initial publish

---
+++ 24. Swap Nodes in Pairs.c
@@ -0,0 +1,53 @@
+/*
+24. Swap Nodes in Pairs
+
+Given a linked list, swap every two adjacent nodes and return its head.
+
+
+
+For example,
+Given 1->2->3->4, you should return the list as 2->1->4->3.
+
+
+
+Your algorithm should use only constant space. You may not modify the values in the list, only nodes itself can be changed.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* swapPairs(struct ListNode* head) {
+    struct ListNode *a, *b = NULL, *c;
+    
+    a = head;
+    if (a) b = a->next;
+    if (b) c = b->next;
+    
+    if (b) {
+        b->next = a;
+        a->next = swapPairs(c);
+        
+        return b;
+    }
+    
+    return a;
+}
+
+
+/*
+Difficulty:Medium
+Total Accepted:181.1K
+Total Submissions:470K
+
+
+Companies Microsoft Bloomberg Uber
+Related Topics Linked List
+Similar Questions 
+                
+                  
+                    Reverse Nodes in k-Group
+*/
