7f39ec3c5230941136ea3523ea89c278ee8f7d4a
Victor <vli02@hotmail.com>
2019-04-02 22:58:07 -0700

    fix typo

--- 9. Palindrome Number.c
+++ 9. Palindrome Number.c
@@ -29,19 +29,19 @@ Coud you solve it without converting the integer to a string?
 */
 
 bool isPalindrome(int x) {
-    int n, d, k = 0;
-    
-    if (x < 0) return false;
-    
-    n = x;
-    while (n) {
-        d = n % 10;
-        if (k > (0x7fffffff - d) / 10) return false;    // overflow
-        k = k * 10 + d;
-        n = n / 10;
-
-    
-    return (k == x);
+    int n, d, k = 0;
+    
+    if (x < 0) return false;
+    
+    n = x;
+    while (n) {
+        d = n % 10;
+        if (k > (0x7fffffff - d) / 10) return false;    // overflow
+        k = k * 10 + d;
+        n = n / 10;
+    }
+    
+    return (k == x);
 }
 
 
