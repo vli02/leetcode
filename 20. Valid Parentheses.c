1a0ab91b7a1fe09bb76f6f1e99c2f84d2fc5fc2e
Victor <vli02@hotmail.com>
2019-04-04 22:08:52 -0700

    change to switch...case.

--- 20. Valid Parentheses.c
+++ 20. Valid Parentheses.c
@@ -7,33 +7,38 @@ The brackets must close in the correct order, "()" and "()[]{}" are all valid bu
 */
 
 bool isValid(char* s) {
-    int l;
-    char *stack;
-    int sp = 0;
-    
-    if (!s || !*s) return true;
-    
-    l = strlen(s);
-    stack = malloc(l * sizeof(char));
-    //assert(stack);
-    while (*s) {
-        if (*s == '(' || *s == '{' || *s == '[') {
-            stack[sp ++] = *s;
-        } else if (sp == 0) {
-            free(stack);
-            return false;
-        } if (*s == ')') {
-            if (stack[-- sp] != '(') { free(stack); return false; }
-        } else if (*s == '}') {
-            if (stack[-- sp] != '{') { free(stack); return false; }
-        } else if (*s == ']') {
-            if (stack[-- sp] != '[') { free(stack); return false; }
-        }
-        s ++;
-    }
-    
-    free(stack);
-    return sp == 0 ? true : false;
+    int l;
+    char *stack, c;
+    int sp = 0;
+    
+    if (!s || !*s) return true;
+    
+    l = strlen(s);
+    stack = malloc(l * sizeof(char));
+    //assert(stack);
+    while (c = *s ++) {
+        switch (c) {
+            case '(':
+            case '{':
+            case '[':
+                stack[sp ++] = c;
+                break;
+            case ')':
+                if (sp == 0 || stack[-- sp] != '(') { free(stack); return false; }
+                break;
+            case '}':
+                if (sp == 0 || stack[-- sp] != '{') { free(stack); return false; }
+                break;
+            case ']':
+                if (sp == 0 || stack[-- sp] != '[') { free(stack); return false; }
+                break;
+            default:
+                break;
+        }
+    }
+    
+    free(stack);
+    return sp == 0 ? true : false;
 }
 
 
