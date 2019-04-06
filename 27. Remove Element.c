3dee6d5623e403b5b9338d326a3bd973e6ca767f
Victor <vli02@hotmail.com>
2019-04-06 15:54:40 -0700

    fix a download problem.

--- 27. Remove Element.c
+++ 27. Remove Element.c
@@ -45,15 +45,15 @@ for (int i = 0; i < len; i++) {
 */
 
 int removeElement(int* nums, int numsSize, int val) {
-    int i, j;
-
-    while (i < numsSize && nums[i] != val) i ++;
-    for (j = i + 1; j < numsSize; j ++) {
-        if (nums[j] != val) {
-            nums[i ++] = nums[j];
-        }
-    }
-    return i;
+    int i, j;
+    i = 0;
+    while (i < numsSize && nums[i] != val) i ++;
+    for (j = i + 1; j < numsSize; j ++) {
+        if (nums[j] != val) {
+            nums[i ++] = nums[j];
+        }
+    }
+    return i;
 }
 
 
