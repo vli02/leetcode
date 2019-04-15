16259d0676a70f29bd1bc80a4d69230deeaa9f3f
Victor <vli02@hotmail.com>
2019-04-14 22:29:36 -0700

    free memory.

--- 32. Longest Valid Parentheses.c
+++ 32. Longest Valid Parentheses.c
@@ -11,60 +11,62 @@ Another example is ")()())", where the longest valid parentheses substring is "(
 */
 
 int longestValidParentheses(char* s) {
-    int i, l, n, *v;
-    int max;
-    
-    if (!s) return 0;
-    
-    l = strlen(s);
-    v = malloc(l * sizeof(int));
-    if (!v) return -1;
-    
-    i = 0; n = 0;
-    while (i < l) {
-        if (s[i] == '(') {
-            v[i] = 1;
-            n ++;
-        } else /*if (s[i] == ')')*/ {
-            if (n > 0) {
-                n --;
-                v[i] = 1;
-            } else {
-                v[i] = 0;
-            }
-        }
-        i ++;
-    }
-    
-    i = l; n = 0;
-    while (i > 0) {
-        i --;
-        if (s[i] == ')') {
-            if (v[i]) {
-                n ++;
-            }
-        } else /*if (s[i] == '(')*/ {
-            if (n > 0) {
-                n --;
-            } else {
-                v[i] = 0;
-            }
-        }
-    }
-    
-    i = 1; n = 0; max = 0;
-    while (i < l) {
-        if (v[i]) {
-            v[i] = v[i - 1] + 1;
-        
-            if (max < v[i]) {
-                max = v[i];
-            }
-        }
-        i ++;
-    }
-    
-    return max;
+    int i, l, n, *v;
+    int max;
+    
+    if (!s) return 0;
+    
+    l = strlen(s);
+    v = malloc(l * sizeof(int));
+    //assert(v);
+    
+    i = 0; n = 0;
+    while (i < l) {
+        if (s[i] == '(') {
+            v[i] = 1;
+            n ++;
+        } else /*if (s[i] == ')')*/ {
+            if (n > 0) {
+                n --;
+                v[i] = 1;
+            } else {
+                v[i] = 0;
+            }
+        }
+        i ++;
+    }
+    
+    i = l; n = 0;
+    while (i > 0) {
+        i --;
+        if (s[i] == ')') {
+            if (v[i]) {
+                n ++;
+            }
+        } else /*if (s[i] == '(')*/ {
+            if (n > 0) {
+                n --;
+            } else {
+                v[i] = 0;
+            }
+        }
+    }
+    
+    i = 1; n = 0; max = 0;
+    while (i < l) {
+        if (v[i]) {
+            v[i] = v[i - 1] + 1;
+        
+            if (max < v[i]) {
+                max = v[i];
+            }
+        }
+        i ++;
+    }
+    
+    free(v);
+    
+    return max;
 }
 
 
