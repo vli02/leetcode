afa8fdfad1429b0a952a562b43a27b40d0b29355
Victor <vli02@hotmail.com>
2019-04-15 23:36:06 -0700

    simplified with a _max() function.

--- 188. Best Time to Buy and Sell Stock IV.c
+++ 188. Best Time to Buy and Sell Stock IV.c
@@ -23,50 +23,50 @@ Explanation: Buy on day 2 (price = 2) and sell on day 3 (price = 6), profit = 6-
              Then buy on day 5 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
 */
 
+int _max(int a, int b) {
+    return a > b ? a : b;
+}
 int all_profits(int* prices, int pricesSize) {
-    int i, d, p;
-        
-    p = 0;
-    for (i = 1; i < pricesSize; i ++) {
-        d = prices[i] - prices[i - 1];
-        p = d > 0 ? p + d : p;  // get it as long as it is a profit!
-    }
-​
-    return p;
+    int i, d, p;
+        
+    p = 0;
+    for (i = 1; i < pricesSize; i ++) {
+        d = prices[i] - prices[i - 1];
+        p = d > 0 ? p + d : p;  // get it as long as it is a profit!
+    }
+
+    return p;
 }
 int maxProfit(int k, int* prices, int pricesSize) {
-    int *b, *s, *buff, i, j, p;
-    
-    if (pricesSize < 2) return 0;
-    
-    if (k >= pricesSize / 2) return all_profits(prices, pricesSize);
-    
-    buff = malloc((2 * k + 1) * sizeof(int));
-    //assert(buff);
-    b = &buff[0];
-    s = &buff[k];
-
-    for (i = 0; i < k; i ++) {
-        b[i] = 0x80000000;  // min integer
-        s[i] = 0;
-    }
-    s[k] = 0;
-    
-    for (i = 0; i < pricesSize; i ++) {
-        for (j = 0; j < k; j ++) {
-            p = s[j] - prices[i];
-            b[j]     = b[j] > p ? b[j] : p;
-            
-            p = b[j] + prices[i];
-            s[j + 1] = s[j + 1] > p ? s[j + 1]  : p;
-        }
-    }
-    
-    p = s[k];
-    
-    free(buff);
-    
-    return p;
+    int *b, *s, *buff, i, j, p;
+    
+    if (pricesSize < 2) return 0;
+    
+    if (k >= pricesSize / 2) return all_profits(prices, pricesSize);
+    
+    buff = malloc((2 * k + 1) * sizeof(int));
+    //assert(buff);
+    b = &buff[0];
+    s = &buff[k];
+    
+    for (i = 0; i < k; i ++) {
+        b[i] = 0x80000000;  // min integer
+        s[i] = 0;
+    }
+    s[k] = 0;
+    
+    for (i = 0; i < pricesSize; i ++) {
+        for (j = 0; j < k; j ++) {
+            b[j]     = _max(b[j],     s[j] - prices[i]);            
+            s[j + 1] = _max(s[j + 1], b[j] + prices[i]);
+        }
+    }
+    
+    p = s[k];
+    
+    free(buff);
+    
+    return p;
 }
 
 
