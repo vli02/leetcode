320982a397ef287169922b43c16e145468fe5990
Victor <vli02@hotmail.com>
2019-04-21 16:01:39 -0700

    fix to pass new test cases.

--- 282. Expression Add Operators.c
+++ 282. Expression Add Operators.c
@@ -20,80 +20,84 @@ Credits:Special thanks to @davidtan1890 for adding this problem and creating all
  * Note: The returned array must be malloced, assume caller calls free().
  */
 typedef struct {
-    char **p;
-    int sz;
-    int n;
+    char **p;
+    int sz;
+    int n;
 } res_t;
 int str2int(char *num, int l) {
-    int k, i;
-    k = 0;
-    while (l -- > 0) {
-        i = *num - '0';
-        if (k > 0xffffffff - i) {
-            return -1;
-        }
-        k = k * 10 + i;
-        num ++;
-    }
-    return k;
+    int k, i;
+    k = 0;
+    while (l -- > 0) {
+        i = *num - '0';
+        if (k > 214748364 ||
+            (k == 214748364 && i > 7)) {
+            return -1;
+        }
+        k = k * 10 + i;
+        num ++;
+    }
+    return k;
 }
 void add2res(char *buff, res_t *res) {
-    if (res->sz == res->n) {
-        res->sz *= 2;
-        res->p = realloc(res->p, res->sz * sizeof(char *));
-        //assert(res->p);
-    }
-    res->p[res->n ++] = strdup(buff);
+    if (res->sz == res->n) {
+        res->sz *= 2;
+        res->p = realloc(res->p, res->sz * sizeof(char *));
+        //assert(res->p);
+    }
+    res->p[res->n ++] = strdup(buff);
 }
-void bt(char *num, int s, int e, int n, int m, int target, char *buff, int d, res_t *res) {
-    int l, k;
-    
-    if (s == e) {  // start == end, done!
-        if (n == target) {
-            buff[d] = 0;
-            add2res(buff, res);
-        }
-        return;
-    }
-    
-    for (l = 1; l <= e - s && l <= 10; l ++) {
-        if (l > 1 && num[s] == '0') break;
-        k = str2int(&num[s], l);
-        if (k < 0) break;
-        if (!d) {
-            strncpy(buff, &num[s], l);
-            bt(num, s + l, e, k, k, target, buff, l, res);
-        } else {
-            strncpy(&buff[d + 1], &num[s], l);
-            buff[d] = '+';
-            bt(num, s + l, e, n + k, k, target, buff, d + 1 + l, res);
-            buff[d] = '-';
-            bt(num, s + l, e, n - k, -k, target, buff, d + 1 + l, res);
-            buff[d] = '*';
-            bt(num, s + l, e, n - m + m * k, m * k, target, buff, d + 1 + l, res);
-        }
-    }
+void bt(char *num, int s, int e, long n, long m, int target, char *buff, int d, res_t *res) {
+    int l, k;
+    
+    if (s == e) {  // start == end, done!
+        if (n == target) {
+            //printf("%d, %d\n", n, target);
+            buff[d] = 0;
+            add2res(buff, res);
+        }
+        return;
+    }
+    
+    for (l = 1; l <= e - s && l <= 10; l ++) {
+        if (l > 1 && num[s] == '0') break;
+        k = str2int(&num[s], l);
+        if (k == -1) break;     // overflow
+        //printf("%d\n", k);
+        if (!d) {
+            strncpy(buff, &num[s], l);
+            bt(num, s + l, e, k, k, target, buff, l, res);
+        } else {
+            strncpy(&buff[d + 1], &num[s], l);
+            buff[d] = '+';
+            bt(num, s + l, e, n + k, k,             target, buff, d + 1 + l, res);
+            buff[d] = '-';
+            bt(num, s + l, e, n - k, -k,            target, buff, d + 1 + l, res);
+            buff[d] = '*';
+            bt(num, s + l, e, n - m + m * k, m * k, target, buff, d + 1 + l, res);
+            // m is previous k, n is overall total up to previous k
+        }
+    }
 }
 char** addOperators(char* num, int target, int* returnSize) {
-    res_t res;
-    char *buff;
-    int l;
-    
-    res.sz = 10;
-    res.p = malloc(res.sz * sizeof(char *));
-    //assert(res.p);
-    res.n = 0;
-    
-    l = strlen(num);
-    buff = malloc(l * 2 * sizeof(char));
-    //assert(buff);
-    
-    bt(num, 0, l, 0, 0, target, buff, 0, &res);
-    
-    free(buff);
-    
-    *returnSize = res.n;
-    return res.p;
+    res_t res;
+    char *buff;
+    int l;
+    
+    res.sz = 10;
+    res.p = malloc(res.sz * sizeof(char *));
+    //assert(res.p);
+    res.n = 0;
+    
+    l = strlen(num);
+    buff = malloc(l * 2 * sizeof(char));
+    //assert(buff);
+    
+    bt(num, 0, l, 0, 0, target, buff, 0, &res);
+    
+    free(buff);
+    
+    *returnSize = res.n;
+    return res.p;
 }
 
 
