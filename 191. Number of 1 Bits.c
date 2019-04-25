c38ac96fed5b241e6d2058dcd7ed6aace4b66efb
Victor <vli02@hotmail.com>
2019-04-24 18:44:17 -0700

    binary search like solution.

--- 191. Number of 1 Bits.c
+++ 191. Number of 1 Bits.c
@@ -8,13 +8,31 @@ For example, the 32-bit integer ’11' has binary representation 000000000000000
 Credits:Special thanks to @ts for adding this problem and creating all test cases.
 */
 
+int helper(uint32_t n, uint32_t b) {
+    uint64_t k;
+    
+    if (n == 0) return 0;
+    
+    k = ((uint64_t)1 << b) - 1;
+    
+    if (n == k) return b;
+    
+    b >>= 1;
+    
+    return helper(n >> b, b) +
+        helper(n & ((1 << b) - 1), b);
+}
 int hammingWeight(uint32_t n) {
-    int c = 0;
-    while (n) {
-        c ++;
-        n &= (n - 1);
-    }
-    return c;
+#if 0
+    int c = 0;
+    while (n) {
+        c ++;
+        n &= (n - 1);
+    }
+    return c;
+#else
+    return helper(n, 32);
+#endif
 }
 
 
