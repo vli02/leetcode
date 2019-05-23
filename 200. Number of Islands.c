9a58c6b74c0f2e60daffd3196fcfab5505922c53
Victor <vli02@hotmail.com>
2019-05-23 00:00:49 -0700

    add bfs solution.

--- 200. Number of Islands.c
+++ 200. Number of Islands.c
@@ -13,8 +13,9 @@ Answer: 3
 Credits:Special thanks to @mithmatt for adding this problem and creating all test cases.
 */
 
+const int offset[4][2] = { { -1, 0 }, { 1, 0 }, { 0, -1, }, { 0, 1 } };
+#if 0   // dfs
 void cs(char **grid, int i, int j, int gridSize, int *gridColSize) {
-    int offset[4][2] = { { -1, 0 }, { 1, 0 }, { 0, -1, }, { 0, 1 } };
     int x, y, k;
 
     grid[i][j] = 0;
@@ -44,6 +45,80 @@ int numIslands(char** grid, int gridSize, int* gridColSize) {
     }
     return n;
 }
+#else   // bfs
+typedef struct {
+    int *q;
+    int left;
+    int right;
+} q_t;
+void add2q(q_t *q, int x, int y) {
+    q->q[q->right ++] = x;
+    q->q[q->right ++] = y;
+}
+#define IDX(X, Y, M)  ((X) * (M) + (Y))
+void bfs(q_t *qa, q_t *qb, int *v, char type, char **grid, int rowsz, int *colsz, int m, int *k) {
+    int o, i, j, x, y;
+    while (qa->left < qa->right) {
+        i = qa->q[qa->left ++];
+        j = qa->q[qa->left ++];
+        for (o = 0; o < 4; o ++) {
+            x = i + offset[o][0];
+            y = j + offset[o][1];
+            if (x < 0 || x >= rowsz ||
+                y < 0 || y >= colsz[x] ||
+                v[IDX(x, y, m)]) continue;
+            v[IDX(x, y, m)] = 1;
+            if (type == '0') {
+                if (grid[x][y] == '0') add2q(qa, x, y);
+                else {
+                    add2q(qb, x, y);
+                    bfs(qb, qa, v, '1', grid, rowsz, colsz, m, k);
+                }
+            } else {
+                if (grid[x][y] == '0') add2q(qb, x, y);
+                else add2q(qa, x, y);
+            }
+        }
+    }
+    if (type == '1') {
+        (*k) ++;
+        bfs(qb, qa, v, '0', grid, rowsz, colsz, m, k);
+    }
+}
+int numIslands(char** grid, int gridSize, int* gridColSize) {
+    q_t q0 = { 0 }, q1 = { 0 };
+    int *v, m, t, i, k;
+    
+    if (gridSize == 0) return 0;
+    
+    for (i = 0, m = 0, t = 0; i < gridSize; i ++) {
+        k = gridColSize[i];
+        if (m < k) m = k;
+        t += k;
+    }
+    
+    q0.q = malloc(t * 2 * sizeof(q0.q[0]));
+    q1.q = malloc(t * 2 * sizeof(q1.q[0]));
+    v = calloc(gridSize * m, sizeof(int));
+    //assert(q0.q && q1.q && v);
+    
+    k = 0;
+    v[IDX(0, 0, m)] = 1;
+    if (grid[0][0] == '0') {
+        add2q(&q0, 0, 0);
+        bfs(&q0, &q1, v, '0', grid, gridSize, gridColSize, m, &k);
+    } else {
+        add2q(&q1, 0, 0);
+        bfs(&q1, &q0, v, '1', grid, gridSize, gridColSize, m, &k);
+    }
+    
+    free(q0.q);
+    free(q1.q);
+    free(v);
+    
+    return k;
+}
+#endif
 
 
 /*
