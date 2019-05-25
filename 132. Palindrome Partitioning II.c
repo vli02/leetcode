98054c704ea01be7610f6fa8a87c7cfc814c0226
Victor <vli02@hotmail.com>
2019-05-24 23:34:48 -0700

    reorganize.

--- 132. Palindrome Partitioning II.c
+++ 132. Palindrome Partitioning II.c
@@ -12,42 +12,46 @@ Return 1 since the palindrome partitioning ["aa","b"] could be produced using 1 
 */
 
 int _min(int a, int b) {
-    return a < b ? a : b;
+    return a < b ? a : b;
 }
 int minCut(char* s) {
-    int *dp, n, i, k;
-    
-    n = strlen(s);
-    
-    dp = malloc((n + 1) * sizeof(int));
-    //assert(dp);
-    
-    for (i = 0; i <=n; i ++) {
-        dp[i] = i - 1;
-    }
-    
-    for (i = 0; i < n; i ++) {
-        for (k = 0;
-             i - k >= 0 &&
-             i + k < n &&
-             s[i - k] == s[i + k];
-             k ++) {
-            dp[i + k + 1] = _min(dp[i + k + 1], dp[i - k] + 1);
-        }
-        for (k = 1;
-             i - k + 1 >= 0 &&
-             i + k < n &&
-             s[i - k + 1] == s[i + k];
-             k ++) {
-            dp[i + k + 1] = _min(dp[i + k + 1], dp[i - k + 1] + 1);
-        }
-    }
-    
-    i = dp[n];
-    
-    free(dp);
-    
-    return i;
+    int *dp, n, i, k;
+    
+    n = strlen(s);
+    
+    dp = malloc((n + 1) * sizeof(int));     // number of cuts on length
+    //assert(dp);
+    
+    dp[0] = -1;
+    for (i = 0; i < n; i ++) {
+        dp[i + 1] = dp[i] + 1;
+    }
+    
+    for (i = 0; i < n; i ++) {
+        dp[i + 1] = _min(dp[i + 1], dp[i] + 1);
+        
+        for (k = 1;                 // "aba"
+             i - k >= 0 &&
+             i + k < n &&
+             s[i - k] == s[i + k];
+             k ++) {
+            dp[i + k + 1] = _min(dp[i + k + 1], dp[i - k] + 1);
+        }
+        
+        for (k = 1;                 // "aaaa"
+             i - k + 1 >= 0 &&
+             i + k < n &&
+             s[i - k + 1] == s[i + k];
+             k ++) {
+            dp[i + k + 1] = _min(dp[i + k + 1], dp[i - k + 1] + 1);
+        }
+    }
+    
+    i = dp[n];
+    
+    free(dp);
+    
+    return i;
 }
 
 
