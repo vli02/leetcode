20bdd34c6b20bee9031da1602f5179867217a515
Victor <vli02@hotmail.com>
2019-06-02 15:32:23 -0700

    simplified a little.

--- 523. Continuous Subarray Sum.c
+++ 523. Continuous Subarray Sum.c
@@ -27,63 +27,50 @@ You may assume the sum of all the numbers is in the range of a signed 32-bit int
 */
 
 typedef struct e_s {
-    int mod;
-    int idx;
-    struct e_s *shadow;
+    int mod;
+    int idx;
+    struct e_s *shadow;
 } e_t;
 #define SZ 1024
-void free_t(e_t **set) {
-    e_t *e, *s;
-    int i;
-    for (i = 0; i < SZ; i ++) {
-        e = set[i];
-        while (e) {
-            s = e->shadow;
-            free(e);
-            e = s;
-        }
-    }
-}
 e_t *lookup(e_t **set, int mod) {
-    e_t *e = set[mod % SZ];
-    while (e && e->mod != mod) {
-        e = e->shadow;
-    }
-    return e;
+    e_t *e = set[mod % SZ];
+    while (e && e->mod != mod) {
+        e = e->shadow;
+    }
+    return e;
 }
-void put(e_t **set, int mod, int idx) {
-    e_t *e = malloc(sizeof(e_t));
-    //assert(e);
-    e->mod = mod;
-    e->idx = idx;
-    e->shadow = set[mod % SZ];
-    set[mod % SZ] = e;
+void put(e_t **set, e_t *e, int mod, int idx) {
+    e->mod = mod;
+    e->idx = idx;
+    e->shadow = set[mod % SZ];
+    set[mod % SZ] = e;
 }
 bool checkSubarraySum(int* nums, int numsSize, int k) {
-    int i, s, found = 0;
-    e_t *set[SZ] = { 0 }, *e;
-    
-    put(set, 0, -1);
-    
-    s = 0;
-    for (i = 0; i < numsSize; i ++) {
-        s += nums[i];
-        if (k) s = s % k;
-        e = lookup(set, s);
-        if (e) {
-            if (i - e->idx >= 2) {
-                found = 1;
-                break;
-            }
-        } else {
-            put(set, s, i);
-        }
-    }
-    
-done:
-    free_t(set);
-    
-    return found;
+    int i, s, found = 0;
+    
+    e_t buff[10000];
+    int n;
+    
+    e_t *set[SZ] = { 0 }, *e;
+    
+    put(set, &buff[n ++], 0, -1);
+    
+    s = 0;
+    for (i = 0; i < numsSize; i ++) {
+        s += nums[i];
+        if (k) s = s % k;
+        e = lookup(set, s);
+        if (e) {
+            if (i - e->idx >= 2) {
+                found = 1;
+                break;
+            }
+        } else {
+            put(set, &buff[n ++], s, i);
+        }
+    }
+    
+    return found;
 }
 
 
